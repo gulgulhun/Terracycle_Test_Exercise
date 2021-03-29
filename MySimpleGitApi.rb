d7462e97fe6d325fafa1944929fe1ce01ec0707e
Tom-Tom <gulgulhun@gmail.com>
2021-03-29 09:42:11 +0200

    Bugfix
    Update OAuth Token
    Fixing bug in the main function

--- MySimpleGitApi.rb
+++ MySimpleGitApi.rb
@@ -1,10 +1,11 @@
 require 'octokit'
 
-#temp github api token: 7b20b36519ecd6349645821d752702672cf39b69
+#temp github api token: a9d65691841e987ed8fb91ad1c8cd084d82dd0e9
 class MySimpleGitApi
 
   def self.get_bad_pr
-    client = Octokit::Client.new(:access_token => "7b20b36519ecd6349645821d752702672cf39b69", per_page: 300)
+    puts "Searching..."
+    client = Octokit::Client.new(:access_token => "a9d65691841e987ed8fb91ad1c8cd084d82dd0e9", :per_page => 300)
 
     end_result = []
     repo = 'rails/rails'
@@ -18,7 +19,8 @@ class MySimpleGitApi
           sus_commits << client.commit(repo, commit.sha)
         end
         filenames = sus_commits.map{|c| c.files.map(&:filename)}.flatten
-        if filenames.detect{|e| filenames.count(e) > 1} != nil && client.pull_files(repo, pull.number).map(&:patch).flatten.map{|d| d.split('@@').select{|s| s.length >10 && s.length < 25}}.flatten.length < sus_commits.map{|c|c.files.map(&:patch)}.flatten.map{|d| d.split('@@').select{|s| s.length > 10 && s.length < 25}}.flatten.length
+        #if the suspect commits modified rows number is more than the pull modified rows number, then the same rows is modifield in more than one commit
+        if filenames.detect{|e| filenames.count(e) > 1} != nil && client.pull_files(repo, pull.number).map(&:patch).flatten.filter_map{|d| d.split('@@').select{|s| s.length >10 && s.length < 25} unless d.nil?}.flatten.length < sus_commits.map{|c|c.files.map(&:patch)}.flatten.filter_map{|d| d.split('@@').select{|s| s.length > 10 && s.length < 25} unless d.nil?}.flatten.length
           end_result << pull.html_url
         end
       end
